1f7dd070a1d936d996df29b64059ba8ec3f8f1f6
qusee <somelelequse03@icloud.com>
2025-06-05 02:05:16 +0200

    Dashboard Metrics

--- src/components/dashboard/dashboard-metrics.tsx
+++ src/components/dashboard/dashboard-metrics.tsx
@@ -1,32 +1,58 @@
-import MetricBlock from "@/components/dashboard/metrics/metric-block";
+import MetricBlock, {MetricBlockProps} from "@/components/dashboard/metrics/metric-block";
 import {BedSingle, Stethoscope, UserRoundCheck, Users2Icon} from "lucide-react";
 
+
+
 const DashboardMetrics = () => {
+
+    const metrics:MetricBlockProps[] = [
+        {
+            title: "Visitors",
+            value: "4,592",
+            icon: <UserRoundCheck size={50} className="text-black bg-[#f9f9f9] p-3 shadow-lg rounded-full" />,
+            description: "Stay informed with real-time data to enhance patient care and visitor management."
+        },
+        {
+            title: "Doctors",
+            value: "260",
+            icon: <Stethoscope size={50} className="text-black bg-[#f9f9f9] p-3 shadow-lg rounded-full" />,
+            description: "Stay updated with essential details to streamline " +
+                "medical support and management."
+        },
+        {
+            title: "Patients",
+            value: "540",
+            icon: <Users2Icon size={50} className="text-black bg-[#f9f9f9] p-3 shadow-lg rounded-full" />,
+            description: "Keep track of patient information at a glance, with easy access to key " +
+                "details for personalised care"
+        },
+        {
+            title: "Total Bed",
+            value: "1205",
+            icon: <BedSingle size={50} className="text-black bg-[#f9f9f9] p-3 shadow-lg rounded-full" />,
+            forBeds: true,
+            general: 215,
+            private: 110,
+        }
+    ]
+
     return (
         <section className="grid grid-cols-4 gap-10 px-10 w-full">
-            <MetricBlock
-                title="Visitors"
-                value="4,592"
-                icon={<UserRoundCheck size={50} className="text-black bg-[#f9f9f9] p-3 shadow-lg rounded-full" />}
-            />
-            <MetricBlock
-                title="Doctors"
-                value="260"
-                icon={<Stethoscope size={50} className="text-black bg-[#f9f9f9] p-3 shadow-lg rounded-full" />}
-            />
-            <MetricBlock
-                title="Patients"
-                value="540"
-                icon={<Users2Icon size={50} className="text-black bg-[#f9f9f9] p-3 shadow-lg rounded-full" />}
-            />
-            <MetricBlock
-                title="Total Bed"
-                value="1205"
-                icon={<BedSingle size={50} className="text-black bg-[#f9f9f9] p-3 shadow-lg rounded-full" />}
-                forBeds={true}
-                general={215}
-                private={110}
-            />
+            {
+                metrics.map((metric, index) => (
+                    <MetricBlock
+                        key={index}
+                        title={metric.title}
+                        value={metric.value}
+                        icon={metric.icon}
+                        forBeds={metric.forBeds}
+                        general={metric.general}
+                        private={metric.private}
+                        totalAvailable={metric.totalAvailable}
+                        description={metric.description}
+                    />
+                ))
+            }
         </section>
     )
 }
--- src/components/dashboard/metrics/metric-block.tsx
+++ src/components/dashboard/metrics/metric-block.tsx
@@ -2,7 +2,7 @@ import {ArrowUp, Ellipsis} from "lucide-react";
 import {JSX} from "react";
 
 
-interface MetricBlockProps {
+export interface MetricBlockProps {
     title: string,
     value: string,
     icon: JSX.Element,
@@ -28,22 +28,32 @@ const MetricBlock = (props: MetricBlockProps) => {
             <section className="flex gap-5 items-center">
                 <p className="text-4xl font-bold">{props.value}</p>
                 {
+                    /**
+                     * If it's not for beds, then show the percentage.
+                     * */
                     !props.forBeds && (<p className="font-medium bg-[#f9f9f9] px-2 rounded-full text-sm flex items-center"><ArrowUp size={15} />15.5%</p>)
                 }
                 {
+                    /**
+                     * If it's for beds, then show the total available.
+                     * */
                     props.forBeds && (<p className="font-medium text-sm flex items-center">{props.totalAvailable} Available</p>)
                 }
             </section>
             {/*Third row of the block*/}
             <section className="flex h-full gap-5 items-center">
                 {
+                    /**
+                     * If it's not for beds, then show the description.
+                     * */
                     !props.forBeds && (<p className="text-sm">
-                        Stay updated with essential details
-                        to streamline medical
-                        support and management.
+                        {props.description}
                     </p>)
                 }
                 {
+                    /**
+                     * If it's for beds, then show the beds.
+                     * */
                     props.forBeds
                     && (<div className="flex justify-between h-full w-full gap-5 items-center">
                         <p className="text-2xl leading-5">
